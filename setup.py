62aa0ea843d8e22c414ea41c14e370c5ca98a8d3
keerthikamoorthy <51756200+keerthikamoorthy@users.noreply.github.com>
2021-11-25 16:46:01 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='test_icomic',
+    version='0.0.1',
+    entry_points={
+        'console_script': [
+            'test_icomic=test_icomic:run'
+            ]})
