6cb62a86e5e974eb1a50a6c36fce7908d0c51ae7
keerthikamoorthy <51756200+keerthikamoorthy@users.noreply.github.com>
2021-11-26 21:16:10 +0530

    Add files via upload

---
+++ icomic/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import os
+with open('requirements.txt') as f:
+      required = f.read().splitlines()
+setup(name='icomic',
+      version='0.1',
+      description='icomic gui',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='GUI toolkit',
+      install_requires= required,
+      entry_points={
+          'console_scripts': ['icomic=mainwin_v35.py:main'],
+      },
+      include_package_data=True,
+      zip_safe=False)
